6c1527ae2862022a9728ec31a09e3b90d7fed11b
xhuang26 <huanglisa@wirelessprv-10-192-238-150.near.illinois.edu>
2016-05-19 12:20:09 -0500

    add done function

--- test/test.js
+++ test/test.js
@@ -1,26 +1,28 @@
 var expect = require('chai').expect;
-var webdriver = require('selenium-webdriver');
+//var webdriver = require('selenium-webdriver');
 var username = process.env.SAUCE_USERNAME;
-var accessKey = process.env.SAUCE_ACCESS_KEY;
-var tunnel_identifier = process.env.TRAVIS_JOB_NUMBER;
-var hub_url = "http://" + username + ":" + accessKey + "@ondemand.saucelabs.com:4445/wd/hub";
+//var accessKey = process.env.SAUCE_ACCESS_KEY;
+//var tunnel_identifier = process.env.TRAVIS_JOB_NUMBER;
+//var hub_url = "http://" + username + ":" + accessKey + "@ondemand.saucelabs.com:4445/wd/hub";
 //var hub_url = 'http://xhuang62:3bbbe590-919b-4bb0-a209-849d8af84776@localhost:4445/wd/hub';
-console.log(hub_url);
-/*var client = require('webdriverjs').remote({
-    port: 4445,
+//console.log(hub_url);
+var client = require('webdriverjs').remote({
+   //port: 4445,
    desiredCapabilities: {
-       'browserName': 'chrome',
+       /*'browserName': 'chrome',
        'platform': 'Windows XP',
-       'version': '43.0',
-       'tunnelIdentifier': tunnel_identifier
+       'version': '43.0'
+       'tunnelIdentifier': tunnel_identifier*/
+       browserName: 'phantomjs'
    },
-    logLevel: 'silent',
-   
+    logLevel: 'silent'
+    //commandExecutor: hub_url
 });
 client.init();
 
-client.url('http://localhost:3000');*/
-var client = new webdriver.Builder().
+client.url('http://localhost:4000');
+
+/*var client = new webdriver.Builder().
   withCapabilities({
     'browserName': 'chrome',
     'platform': 'Windows XP',
@@ -31,9 +33,12 @@ var client = new webdriver.Builder().
   }).
   usingServer(hub_url).
   build();
-client.get('https://www.google.com');
+client.get('https://www.google.com');*/
 
 describe('simple test', function(){
+    before(function(done) {
+        client.init().url('http://localhost:4000', done);
+    });
     describe('naive equal test', function(){
       it('1 equals to 1', function(done){
           var foo = 1;
@@ -43,22 +48,21 @@ describe('simple test', function(){
     });
     describe('Check homepage', function(){
         it('should see the correct title', function(done) {
-            /*client.getTitle(function(err, title){
-                console.log("error is: "+ err);
-                console.log("title is:" + title);
+            client.getTitle(function(err, title){
                 expect(title).to.have.string('Visualize');
                 done();
-            });*/
-            client.getTitle().then(function (title) {
+            });
+            done();
+            /*client.getTitle().then(function (title) {
                 console.log("title is: " + title);
                 expect(title).to.have.string('Google');
                 done();
-            });
+            });*/
         });
     });
     
     after(function(done) {
-        client.quit();
+        client.end();
         done();
     });
 });
