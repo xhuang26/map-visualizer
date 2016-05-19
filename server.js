b11953fc83a7ba80c8515e4244e08a37dd16edab
xhuang26 <huanglisa@wirelessprv-10-192-238-150.near.illinois.edu>
2016-05-19 12:08:29 -0500

    try to run selenium locally

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-port = process.argv[2] || 3000;
+port = process.argv[2] || 4000;
 
 app.use(express.static(__dirname));
 app.listen(port); //the port you want to use
