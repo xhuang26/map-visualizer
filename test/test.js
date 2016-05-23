56323bacfa49b506bf2f782bbbdc1345ff3b2b20
xhuang26 <huanglisa@wirelessprvnat-172-17-197-211.near.illinois.edu>
2016-05-23 14:26:45 -0500

    describe more test cases

--- test/test.js
+++ test/test.js
@@ -24,7 +24,6 @@ describe('simple test', function(){
     before(function() {
         
         browser
-            //.init()
             .url('http://localhost:4000');
     });
     describe('Check homepage when first loaded', function(){
@@ -33,7 +32,7 @@ describe('simple test', function(){
             expect(title).to.equal('Visualize');
            
         });
-        it('should have map with the size same as viewport', function(done){
+        it('should have map with the size same as viewport', function(){
             var viewportHeight = browser.getViewportSize('height')+"px";
             var viewportWidth = browser.getViewportSize('width')+"px";
             var mapWidth = browser.getCssProperty('#map', 'width').value;
@@ -41,11 +40,10 @@ describe('simple test', function(){
             expect(mapWidth).to.equal(viewportWidth);
             expect(mapHeight).to.equal(viewportHeight);
         })
-        it('should have list hidden', function(done){
-            var judge= browser.isExisting('#map div');
-            console.log(judge);
-            browser.pause(100000);
-            done();
+        it('should have list hidden', function(){
+                expect(browser.isExisting('#map .ol-viewport')).to.equal(true);
+                expect(browser.isExisting('#map .ol-viewport .layer-list')).to.equal(true);
+                expect(browser.getCssProperty('.layer-list', 'width').value).to.equal('0px');
                 /*.element('#notifications').then(function(res){
                     console.log(res.value);
                 })
@@ -59,9 +57,44 @@ describe('simple test', function(){
         });
         
     });
+    describe('should have layers button work noramally', function(){
+        it('should have "layer" in text', function(){
+            
+        });
+        it('should span the list when click', function(){
+            
+        });
+        it('should have list hidden when click again', function(){
+            
+        });
+    });
+    describe('should deal with different cases about source loading', function(){
+        //try to explore as much cases as possible
+        describe('should catch error when invalid source file url included', function(){
+            
+        });
+        describe('should catch error when invalid json file included', function(){
+            
+        });
+        describe('should catch error when source file data does not pass validity test', function(){
+            
+        });
+        describe('should catch error when invalid config string included', function(){
+            
+        });
+        describe('should catch err when config string contains invalid data', function(){
+            
+        });
+        describe('should catch error when invalid extent string included', function(){
+            
+        });
+        describe('should catch error when extent string contains invalid data', function(){
+            
+        });
+    });
+    
    after(function(done) {
         browser
-            //.end()
             .call(done);
     });
 });
